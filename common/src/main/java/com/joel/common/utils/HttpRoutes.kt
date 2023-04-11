72dd4b546b783ea5fbaff674b0f04b22adbcbb78
Joel-K-Muraguri <joelkmuraguri21@gmail.com>
2023-04-11 17:58:15 +0300

    constants

---
+++ common/src/main/java/com/joel/common/utils/HttpRoutes.kt
@@ -0,0 +1,12 @@
+package com.joel.common.utils
+
+object HttpRoutes {
+
+    const val RANDOM_URL = "https://api.spoonacular.com/recipes/random?apiKey=77581e09933d4bf8bb0c68e124e02b73"
+    const val BASE_URL = "https://api.spoonacular.com"
+    const val RECIPE_IMAGE_URL = "https://spoonacular.com/recipeImages/11265-636x393.jpg"
+    const val IMAGE_URL = "https://spoonacular.com/"
+    const val API_KEY = "77581e09933d4bf8bb0c68e124e02b73"
+    const val PARAM_GAME_ID = 0
+
+}
